7c86c0ab38a07fb2340fbf9d9f416fbbd986c1e5
Carson Gross <carsongross@gmail.com>
2010-12-19 19:21:02 -0800

    Move init logic to the, er, init() method

--- ronin/src/ronin/RoninServletWrapper.java
+++ ronin/src/ronin/RoninServletWrapper.java
@@ -13,6 +13,7 @@ import gw.lang.reflect.ReflectUtil;
 import gw.lang.reflect.TypeSystem;
 import gw.lang.shell.Gosu;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -30,60 +31,52 @@ import java.util.List;
  */
 public class RoninServletWrapper extends HttpServlet {
 
-  private volatile boolean _init;
   private HttpServlet _roninServlet;
 
   @Override
   protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    initGosu(req);
     _roninServlet.service(req, resp);
   }
 
-  private void initGosu(HttpServletRequest req) throws ServletException {
-    if (!_init) {
-      synchronized (RoninServletWrapper.class) {
-        if (!_init) {
-          String strServletDir = req.getSession().getServletContext().getRealPath("/");
-          File servletDir = new File(strServletDir);
-          final List<File> classpath = new ArrayList<File>();
-          File resourceRoot = determineRoot(servletDir);
-          if (resourceRoot.isDirectory()) {
-            File classes = new File(resourceRoot, "classes");
-            classpath.add(classes);
-            File src = new File(resourceRoot, "src");
-            classpath.add(src);
-            File lib = new File(resourceRoot, "lib");
-            if (lib.isDirectory()) {
-              //noinspection ResultOfMethodCallIgnored
-              lib.listFiles(
-                      new FilenameFilter() {
-                        @Override
-                        public boolean accept(File dir, String name) {
-                          String lname = name.toLowerCase();
-                          if (lname.endsWith(".jar") || lname.endsWith(".zip")) {
-                            if (lname.endsWith("ronin.jar")) {
-                              // workaround for http://code.google.com/p/gosu-lang/issues/detail?id=2
-                              addRoninJar( dir, name, classpath );
-                            } else {
-                              classpath.add(new File(dir, name));
-                            }
-                          }
-                          return false;
-                        }
-                      });
+  @Override
+  public void init(ServletConfig config) throws ServletException {
+    String strServletDir = config.getServletContext().getRealPath("/");
+    File servletDir = new File(strServletDir);
+    final List<File> classpath = new ArrayList<File>();
+    File resourceRoot = determineRoot(servletDir);
+    if (resourceRoot.isDirectory()) {
+      File classes = new File(resourceRoot, "classes");
+      classpath.add(classes);
+      File src = new File(resourceRoot, "src");
+      classpath.add(src);
+      File lib = new File(resourceRoot, "lib");
+      if (lib.isDirectory()) {
+        //noinspection ResultOfMethodCallIgnored
+        lib.listFiles(
+          new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+              String lname = name.toLowerCase();
+              if (lname.endsWith(".jar") || lname.endsWith(".zip")) {
+                if (lname.endsWith("ronin.jar")) {
+                  // workaround for http://code.google.com/p/gosu-lang/issues/detail?id=2
+                  addRoninJar(dir, name, classpath);
+                } else {
+                  classpath.add(new File(dir, name));
+                }
+              }
+              return false;
             }
-          }
-          Gosu.initGosu(null, classpath);
-
-          //TODO cgross - remove me when we properly load typeloaders out of non java-classpath jars
-          TypeSystem.pushGlobalTypeLoader( (ITypeLoader)ReflectUtil.construct( "ronindb.DBTypeLoader" ) );
-
-          _roninServlet = (HttpServlet) ReflectUtil.construct( "ronin.RoninServlet", "true".equals(System.getProperty("dev.mode")));
-          _roninServlet.init(getServletConfig());
-          _init = true;
-        }
+          });
       }
     }
+    Gosu.initGosu(null, classpath);
+
+    //TODO cgross - remove me when we properly load typeloaders out of non java-classpath jars
+    TypeSystem.pushGlobalTypeLoader((ITypeLoader) ReflectUtil.construct("ronindb.DBTypeLoader"));
+
+    _roninServlet = (HttpServlet) ReflectUtil.construct("ronin.RoninServlet", "true".equals(System.getProperty("dev.mode")));
+    _roninServlet.init(getServletConfig());
   }
 
   private void addRoninJar( File dir, String name, List<File> classpath )
